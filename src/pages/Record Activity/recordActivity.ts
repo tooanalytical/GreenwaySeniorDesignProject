d889bbc51e828c7d27fc8f144ee3c911423552b5
Brandon Young <brandonyoung@Brandons-MacBook-Pro.local>
2017-11-02 00:05:23 -0400

    Initial 'Record Activity' page commit with timer functionality completed.

--- src/pages/Record Activity/recordActivity.ts
+++ src/pages/Record Activity/recordActivity.ts
@@ -1,42 +1,91 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+import { Component, ViewChild, ElementRef } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { Geolocation } from "@ionic-native/geolocation";
 
 declare var google;
 
 @Component({
-  selector: 'page-recordActivity',
-  templateUrl: 'recordActivity.html'
+  selector: "page-recordActivity",
+  templateUrl: "recordActivity.html"
 })
 export class RecordActivityPage {
-
-  @ViewChild('map') mapElement: ElementRef;
+  @ViewChild("map") mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  public timer = "00:00:00";
+  public startButton = "START";
+  public seconds;
+  public minutes;
+  public hours;
+  public start;
+  public default;
+  public timer_id;
 
-  }
+  public addZero = function(value) {
+    if (value < 10) {
+      value = "0" + value;
+    }
+    return value;
+  };
 
-  ionViewDidLoad(){
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {}
+
+  ionViewDidLoad() {
     this.loadMap();
   }
 
-  loadMap(){
+  loadMap() {
+    this.geolocation.getCurrentPosition().then(
+      position => {
+        let latLng = new google.maps.LatLng(
+          position.coords.latitude,
+          position.coords.longitude
+        );
 
-    this.geolocation.getCurrentPosition().then((position) => {
+        let mapOptions = {
+          center: latLng,
+          zoom: 15,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
 
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        this.map = new google.maps.Map(
+          this.mapElement.nativeElement,
+          mapOptions
+        );
+      },
+      err => {
+        console.log(err);
       }
+    );
+  }
 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  startStop() {
+    if (this.startButton === "Start") {
+      this.start = new Date();
+      this.timer_id = setInterval(() => {
+        this.seconds = Math.floor(
+          (new Date().getTime() - this.start.getTime()) / 1000
+        );
+        this.minutes = Math.floor(this.seconds / 60);
+        this.hours = Math.floor(this.minutes / 60);
 
-    }, (err) => {
-      console.log(err);
-    });
+        this.seconds = this.seconds - this.minutes * 60;
+        this.minutes = this.minutes - this.hours * 60;
+
+        this.hours = this.addZero(this.hours);
+        this.minutes = this.addZero(this.minutes);
+        this.seconds = this.addZero(this.seconds);
+
+        this.timer = this.hours + ":" + this.minutes + ":" + this.seconds;
+      }, 10);
+
+      this.startButton = "End";
+      this.default = "danger";
+    } else {
+      clearInterval(this.timer_id);
+
+      this.startButton = "Start";
+      this.default = "default";
+    }
   }
 }

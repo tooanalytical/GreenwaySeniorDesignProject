fd5f3b6dd079a674542ab4171ecd7914d89947d0
Brandon Young <brandonyoung@Brandons-iMac.local>
2018-01-14 22:47:17 -0500

    Implemented name validation parameters during first step of account creation process

--- src/pages/Create Account - Name/createAccountName.ts
+++ src/pages/Create Account - Name/createAccountName.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 import { CreateAccountEmailPage } from '../Create Account - Email/createAccountEmail';
 
@@ -9,14 +10,46 @@ import { CreateAccountEmailPage } from '../Create Account - Email/createAccountE
   templateUrl: 'createAccountName.html'
 })
 export class CreateAccountNamePage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {}
 
   data = {
     firstName: '',
     lastName: ''
   };
 
-  createAccountNext(data) {
-    this.navCtrl.push(CreateAccountEmailPage, data);
+  //Validates the user's name fits acceptable parameters
+  nameValidation() {
+    var flag;
+
+    //Validates first and last name name is not empty and contains only letters
+    if(this.data.firstName !== '' && this.data.lastName !== '' && /^[a-zA-Z]+$/.test(this.data.firstName) && /^[a-zA-Z]+$/.test(this.data.lastName)){
+      flag = true;
+    //Sets flag to false if it fails the validation test.
+    } else {
+      flag = false;
+    }
+    return flag;
   }
+
+  //Presents a modal alert to the user if they violate name parameters.
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Uh oh!',
+      subTitle: 'Please make sure your name is not blank and only contains letters.',
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
+  //Pushes user to next step in account creation process if their name passes validation.
+  createAccountNext(data) {
+    if(this.nameValidation()) {
+      this.navCtrl.push(CreateAccountEmailPage, data);
+    } else {
+      this.presentAlert();
+    }
+    
+  }
+
+  
 }

366e1bcd1437dd4b9af4c6d4eed52367ac03df3b
Brandon Young <brandonyoung@Brandons-iMac.local>
2018-01-15 23:47:43 -0500

    Implemented initial email address validation. Needs testing.

--- src/pages/Create Account - Email/createAccountEmail.ts
+++ src/pages/Create Account - Email/createAccountEmail.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 import { CreateAccountPasswordPage } from '../Create Account - Password/createAccountPassword';
 
@@ -9,7 +10,7 @@ import { CreateAccountPasswordPage } from '../Create Account - Password/createAc
   templateUrl: 'createAccountEmail.html'
 })
 export class CreateAccountEmailPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {}
 
   firstName = this.navParams.get('firstName');
   lastName = this.navParams.get('lastName');
@@ -20,7 +21,36 @@ export class CreateAccountEmailPage {
     emailAddress: ''
   };
 
+  //Validates the user's email address fits acceptable parameters
+  emailValidation() {
+    var flag;
+
+    //Validates the email is not empty and consists of a correct email format.
+    if(this.data.emailAddress !== '' && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.data.emailAddress)){
+      flag = true;
+    //Sets flag to false if it fails the validation test.
+    } else {
+      flag = false;
+    }
+    return flag;
+  }
+
+  //Presents a modal alert to the user if they violate email parameters.
+  presentAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Uh oh!',
+      subTitle: 'Please enter a valid email address.',
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
+
   createAccountNext() {
-    this.navCtrl.push(CreateAccountPasswordPage, this.data);
+    if(this.emailValidation()) {
+      this.navCtrl.push(CreateAccountPasswordPage, this.data);
+    } else {
+      this.presentAlert();
+    }
+    
   }
 }

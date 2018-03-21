2847ac56daad5b05230cedb4ec4338d563e48ed5
Brandon Young <brandon.young1@gmail.com>
2018-03-21 16:58:04 -0400

    Initial implementation of the Account Statistics page. Cleanup and error fixes required.

--- src/pages/Account Statistics/accountStatistics.ts
+++ src/pages/Account Statistics/accountStatistics.ts
@@ -1,26 +1,86 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-accountStatistics',
   templateUrl: 'accountStatistics.html'
 })
 export class AccountStatisticsPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http: Http,
+    public storage: Storage
+  ) {
+    this.userStatisticData.response = '';
+    this.http = http;
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(AccountStatisticsPage, {
-      item: item
+  public userStatisticData = {
+    userId: this.storage.get('userId').then(val => {
+      this.userStatisticData.userId = val;
+    }),
+    totalDistanceWalking: '',
+    totalDistanceRunning: '',
+    totalDistanceCycling: '',
+    totalDistanceTraveled: '',
+    totalCaloriesWalking: '',
+    totalCaloriesRunning: '',
+    totalCaloriesCycling: '',
+    totalCaloriesBurned: '',
+    hazardsReported: '',
+    hazardsRemoved: '',
+    response: ''
+  };
+
+  ionViewWillEnter() {
+    this.getUserStatistics();
+  }
+
+  // Sends the UserId to the server and receives the activityId returned
+  getUserStatistics(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      console.log('getUserStatistics Called');
+      var link =
+        'https://virdian-admin-portal-whitbm06.c9users.io/Mobile_Connections/get_statistics.php';
+      var myData = JSON.stringify({
+        userId: this.userStatisticData.userId
+      });
+      console.log('Calling post');
+      this.http.post(link, myData).subscribe(
+        data => {
+          var response = data['_body'];
+
+          var rawReturn = JSON.parse(response);
+
+          this.userStatisticData.totalDistanceWalking =
+            rawReturn.totalDistance_Walking;
+          this.userStatisticData.totalDistanceRunning =
+            rawReturn.totalDistance_Running;
+          this.userStatisticData.totalDistanceCycling =
+            rawReturn.totalDistance_Biking;
+          this.userStatisticData.totalDistanceTraveled =
+            rawReturn.totalDistance_Overall;
+          this.userStatisticData.totalCaloriesWalking =
+            rawReturn.totalCalories_Walking;
+          this.userStatisticData.totalCaloriesRunning =
+            rawReturn.totalCalories_Running;
+          this.userStatisticData.totalCaloriesCycling =
+            rawReturn.totalCalories_Biking;
+          this.userStatisticData.totalCaloriesBurned =
+            rawReturn.totalCalories_Overall;
+          this.userStatisticData.hazardsReported = rawReturn.hazards_reported;
+          this.userStatisticData.hazardsRemoved = rawReturn.hazards_removed;
+
+          return resolve(response);
+        },
+        error => {
+          console.log('Oooops!');
+          return reject(error);
+        }
+      );
     });
   }
 }

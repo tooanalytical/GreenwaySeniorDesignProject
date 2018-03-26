9081453e776eb6514c11069c40a776da7aaf7ad1
Brandon Young <brandon.young1@gmail.com>
2018-03-26 10:20:01 -0400

    Fixes to account statistics.

--- src/pages/Account Statistics/accountStatistics.ts
+++ src/pages/Account Statistics/accountStatistics.ts
@@ -40,96 +40,120 @@ export class AccountStatisticsPage {
   }
 
   // Sends the UserId to the server and receives the activityId returned
-  getUserStatistics(): Promise<any> {
-    return new Promise((resolve, reject) => {
-      console.log('getUserStatistics Called');
-      var link =
-        'https://virdian-admin-portal-whitbm06.c9users.io/Mobile_Connections/get_statistics.php';
-      var myData = JSON.stringify({
-        userId: this.userStatisticData.userId
-      });
-      console.log('Calling post');
-      this.http.post(link, myData).subscribe(
-        data => {
-          var response = data['_body'];
-
-          var rawReturn = JSON.parse(response);
-
-          if (rawReturn.totalDistance_Walking == null) {
-            this.userStatisticData.totalDistanceWalking = '0';
-          } else {
-            this.userStatisticData.totalDistanceWalking =
-              rawReturn.totalDistance_Walking;
-          }
-
-          if (rawReturn.totalDistance_Running == null) {
-            this.userStatisticData.totalDistanceRunning = '0';
-          } else {
-            this.userStatisticData.totalDistanceRunning =
-              rawReturn.totalDistance_Running;
-          }
-
-          if (rawReturn.totalDistance_Biking == null) {
-            this.userStatisticData.totalDistanceCycling = '0';
-          } else {
-            this.userStatisticData.totalDistanceCycling =
-              rawReturn.totalDistance_Biking;
-          }
-
-          if (rawReturn.totalDistance_Overall == null) {
-            this.userStatisticData.totalDistanceTraveled = '0';
-          } else {
-            this.userStatisticData.totalDistanceTraveled =
-              rawReturn.totalDistance_Overall;
-          }
-
-          if (rawReturn.totalCalories_Walking == null) {
-            this.userStatisticData.totalCaloriesWalking = '0';
-          } else {
-            this.userStatisticData.totalCaloriesWalking =
-              rawReturn.totalCalories_Walking;
-          }
-
-          if (rawReturn.totalCalories_Running == null) {
-            this.userStatisticData.totalCaloriesRunning = '0';
-          } else {
-            this.userStatisticData.totalCaloriesRunning =
-              rawReturn.totalCalories_Running;
-          }
-
-          if (rawReturn.totalCalories_Biking == null) {
-            this.userStatisticData.totalCaloriesCycling = '0';
-          } else {
-            this.userStatisticData.totalCaloriesCycling =
-              rawReturn.totalCalories_Biking;
-          }
-
-          if (rawReturn.totalCalories_Overall == null) {
-            this.userStatisticData.totalCaloriesBurned = '0';
-          } else {
-            this.userStatisticData.totalCaloriesBurned =
-              rawReturn.totalCalories_Overall;
-          }
-
-          if (rawReturn.hazards_reported == null) {
-            this.userStatisticData.hazardsReported = '0';
-          } else {
-            this.userStatisticData.hazardsReported = rawReturn.hazards_reported;
-          }
-
-          if (rawReturn.hazards_removed == null) {
-            this.userStatisticData.hazardsRemoved = '0';
-          } else {
-            this.userStatisticData.hazardsRemoved = rawReturn.hazards_removed;
-          }
-
-          return resolve(response);
-        },
-        error => {
-          console.log('Oooops!');
-          return reject(error);
-        }
-      );
+  getUserStatistics() {
+    // return new Promise((resolve, reject) => {
+    console.log('getUserStatistics Called');
+    var link =
+      'https://virdian-admin-portal-whitbm06.c9users.io/Mobile_Connections/get_statistics.php';
+    var myData = JSON.stringify({
+      userId: this.userStatisticData.userId
     });
+    console.log('Calling post');
+    this.http.post(link, myData).subscribe(
+      data => {
+        var response = data['_body'];
+
+        var rawReturn = JSON.parse(response);
+
+        if (rawReturn.totalDistance_Walking === null) {
+          this.userStatisticData.totalDistanceWalking = '0';
+        } else {
+          this.userStatisticData.totalDistanceWalking =
+            rawReturn.totalDistance_Walking;
+          this.userStatisticData.totalDistanceWalking = this.userStatisticData.totalDistanceWalking.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalDistance_Running === null) {
+          this.userStatisticData.totalDistanceRunning = '0';
+        } else {
+          this.userStatisticData.totalDistanceRunning =
+            rawReturn.totalDistance_Running;
+          this.userStatisticData.totalDistanceRunning = this.userStatisticData.totalDistanceRunning.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalDistance_Biking === null) {
+          this.userStatisticData.totalDistanceCycling = '0';
+        } else {
+          this.userStatisticData.totalDistanceCycling =
+            rawReturn.totalDistance_Biking;
+          this.userStatisticData.totalDistanceCycling = this.userStatisticData.totalDistanceCycling.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalDistance_Overall === null) {
+          this.userStatisticData.totalDistanceTraveled = '0';
+        } else {
+          this.userStatisticData.totalDistanceTraveled =
+            rawReturn.totalDistance_Overall;
+          this.userStatisticData.totalDistanceTraveled = this.userStatisticData.totalDistanceTraveled.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalCalories_Walking === null) {
+          this.userStatisticData.totalCaloriesWalking = '0';
+        } else {
+          this.userStatisticData.totalCaloriesWalking =
+            rawReturn.totalCalories_Walking;
+          this.userStatisticData.totalCaloriesWalking = this.userStatisticData.totalCaloriesWalking.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalCalories_Running === null) {
+          this.userStatisticData.totalCaloriesRunning = '0';
+        } else {
+          this.userStatisticData.totalCaloriesRunning =
+            rawReturn.totalCalories_Running;
+          this.userStatisticData.totalCaloriesRunning = this.userStatisticData.totalCaloriesRunning.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalCalories_Biking === null) {
+          this.userStatisticData.totalCaloriesCycling = '0';
+        } else {
+          this.userStatisticData.totalCaloriesCycling =
+            rawReturn.totalCalories_Biking;
+          this.userStatisticData.totalCaloriesCycling = this.userStatisticData.totalCaloriesCycling.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.totalCalories_Overall === null) {
+          this.userStatisticData.totalCaloriesBurned = '0';
+        } else {
+          this.userStatisticData.totalCaloriesBurned =
+            rawReturn.totalCalories_Overall;
+          this.userStatisticData.totalCaloriesBurned = this.userStatisticData.totalCaloriesBurned.match(
+            /^-?\d+(?:\.\d{0,2})?/
+          )[0];
+        }
+
+        if (rawReturn.hazards_reported === null) {
+          this.userStatisticData.hazardsReported = '0';
+        } else {
+          this.userStatisticData.hazardsReported = rawReturn.hazards_reported;
+        }
+
+        if (rawReturn.hazards_removed === null) {
+          this.userStatisticData.hazardsRemoved = '0';
+        } else {
+          this.userStatisticData.hazardsRemoved = rawReturn.hazards_removed;
+        }
+
+        // return resolve(response);
+      },
+      error => {
+        console.log('Oooops!');
+        // return reject(error);
+      }
+    );
+    // });
   }
 }
